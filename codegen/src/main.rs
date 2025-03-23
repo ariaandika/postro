71d157fdfaef77628f5451a5b4178541a50f884d
ariaandika <andikaakun17@gmail.com>
2025-03-23 10:28:22 +0700

    implement codegen for pg_type and pg_range

--- codegen/src/main.rs
+++ codegen/src/main.rs
@@ -15,16 +15,21 @@ const PG_TYPE: &str = include_str!("../pg_type.dat");
 const PG_RANGE: &str = include_str!("../pg_range.dat");
 const ERRCODES: &str = include_str!("../errcodes.txt");
 
+const PG_DEST: &str = "postgres-types/src/type_gen.rs";
 const ERRCODES_DEST: &str = "src/error/sqlstate.rs";
 
 fn main() -> anyhow::Result<()> {
-    let _pg_type = PgTypeCollector::parser(PG_TYPE)?.parse()?;
-    let _pg_range = PgRangeCollector::parser(PG_RANGE)?.parse()?;
+    let types = PgTypeCollector::parser(PG_TYPE)?.parse()?;
+    let ranges = PgRangeCollector::parser(PG_RANGE)?.parse()?;
     let errcodes = ErrCodeParser::new(ERRCODES).parse();
 
-    let mut errcodesrc = BufWriter::new(File::create(ERRCODES_DEST).context(ERRCODES_DEST)?);
-    ErrCodeGen::new(errcodes).codegen(&mut errcodesrc)?;
-    errcodesrc.flush()?;
+    let mut buffer = BufWriter::new(File::create(PG_DEST).context(PG_DEST)?);
+    pg::codegen(types, ranges, &mut buffer)?;
+    buffer.flush()?;
+
+    let mut buffer = BufWriter::new(File::create(ERRCODES_DEST).context(ERRCODES_DEST)?);
+    ErrCodeGen::new(errcodes).codegen(&mut buffer)?;
+    buffer.flush()?;
 
     Ok(())
 }

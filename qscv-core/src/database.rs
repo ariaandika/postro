e1a00de7265585474dc7b53965b0f27306bac6fb
ariaandika <andikaakun17@gmail.com>
2025-03-24 00:47:28 +0700

    add more driver traits

--- qscv-core/src/database.rs
+++ qscv-core/src/database.rs
@@ -1,11 +1,38 @@
 use std::fmt::Debug;
 
-use crate::type_info::TypeInfo;
+use crate::{
+    arguments::Arguments,
+    column::Column,
+    connection::Connection,
+    row::Row,
+    type_info::TypeInfo,
+    value::{Value, ValueRef},
+};
 
 pub trait Database: 'static + Sized + Send + Debug {
+    /// The concrete `Connection` implementation for this database.
+    type Connection: Connection<Database = Self>;
+
+    /// The concrete `Row` implementation for this database.
+    type Row: Row<Database = Self>;
+
+    /// The concrete `Column` implementation for this database.
+    type Column: Column<Database = Self>;
 
     /// The concrete `TypeInfo` implementation for this database.
     type TypeInfo: TypeInfo;
+
+    /// The concrete type used to hold an owned copy of the not-yet-decoded value that was
+    /// received from the database.
+    type Value: Value<Database = Self> + 'static;
+    /// The concrete type used to hold a reference to the not-yet-decoded value that has just been
+    /// received from the database.
+    type ValueRef<'r>: ValueRef<'r, Database = Self>;
+
+    /// The concrete `Arguments` implementation for this database.
+    type Arguments<'q>: Arguments<'q, Database = Self>;
+    /// The concrete type used as a buffer for arguments while encoding.
+    type ArgumentBuffer<'q>;
 
     /// The display name for this database driver.
     const NAME: &'static str;
--- qscv-core/src/lib.rs
+++ qscv-core/src/lib.rs
@@ -11,9 +11,16 @@ pub mod net;
 pub mod error;
 
 // NOTE: driver traits
+pub mod decode;
+pub mod encode;
 pub mod types;
-pub mod database;
 pub mod type_info;
+pub mod database;
+pub mod connection;
+pub mod arguments;
+pub mod row;
+pub mod column;
+pub mod value;
 
 // NOTE: toolkit
 pub mod migrate;

6ef77513b2b643ab37ea26a6841ec79c508ba74f
ariaandika <andikaakun17@gmail.com>
2025-03-24 17:07:10 +0700

    add query family

--- qscv-core/src/lib.rs
+++ qscv-core/src/lib.rs
@@ -10,20 +10,25 @@ pub mod ext;
 pub mod io;
 pub mod net;
 pub mod rt;
+pub mod sync;
+pub mod describe;
 pub mod error;
 
-// NOTE: core
+// NOTE: core traits
 pub mod decode;
 pub mod encode;
 pub mod types;
 pub mod from_row;
-pub mod describe;
-pub mod acquire;
 pub mod executor;
-pub mod sync;
+
+// NOTE: query
+pub mod query;
+pub mod query_as;
+pub mod query_scalar;
 
 // NOTE: driver traits
 pub mod database;
+pub mod acquire;
 pub mod connection;
 pub mod transaction;
 pub mod statement;

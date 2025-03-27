23bfb508b48968eb627c31269d4ef433ffb22043
ariaandika <andikaakun17@gmail.com>
2025-03-27 10:09:48 +0700

    use thiserror for error

--- qscv-postgres/src/error.rs
+++ qscv-postgres/src/error.rs
@@ -1,22 +1,11 @@
-use std::fmt;
-
 pub type Result<T,E = Error> = std::result::Result<T,E>;
 
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
+    #[error("configuration error: {0}")]
     Configuration(String),
+
+    #[error(transparent)]
     Other(Box<dyn std::error::Error + Send + Sync>)
 }
-
-impl std::error::Error for Error { }
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Error::Configuration(e) => fmt::Display::fmt(e, f),
-            Error::Other(e) => fmt::Display::fmt(e, f)
-        }
-    }
-}
-
 

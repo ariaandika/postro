3327f939eb76c7b1d8b05fcd451816c40a040d4e
ariaandika <andikaakun17@gmail.com>
2025-03-30 20:04:22 +0700

    add Debug util for bytes

--- qscv-postgres/src/common/mod.rs
+++ qscv-postgres/src/common/mod.rs
@@ -33,3 +33,32 @@ impl std::fmt::Debug for GeneralError {
     }
 }
 
+/// Copied from `Bytes` `Debug` implementation
+pub struct BytesRef<'a>(pub &'a [u8]);
+impl std::fmt::Debug for BytesRef<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "b\"")?;
+        for &b in self.0 {
+            if b == b'\n' {
+                write!(f, "\\n")?;
+            } else if b == b'\r' {
+                write!(f, "\\r")?;
+            } else if b == b'\t' {
+                write!(f, "\\t")?;
+            } else if b == b'\\' || b == b'"' {
+                write!(f, "\\{}", b as char)?;
+            } else if b == b'\0' {
+                write!(f, "\\0")?;
+            } else if (0x20..0x7f).contains(&b) {
+                write!(f, "{}", b as char)?;
+            } else {
+                write!(f, "\\x{:02x}", b)?;
+            }
+        }
+        write!(f, "\"")?;
+        Ok(())
+    }
+}
+
+
+

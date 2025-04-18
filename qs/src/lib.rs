25a85159615c8b4c1469b7cfd73fd1063c3cae83
ariaandika <andikaakun17@gmail.com>
2025-04-18 20:37:11 +0700

    add little docs

--- qs/src/lib.rs
+++ qs/src/lib.rs
@@ -1,3 +1,37 @@
+//! Postgres Driver
+//!
+//! # Query API
+//!
+//! Query API starts from the [`query`] function, it accepts a sql string, and an [`Executor`].
+//!
+//! An [`Executor`] is a flexible type that represent database connection. For more
+//! information, see the [`executor`] module.
+//!
+//! ## The [`Query`] struct
+//!
+//! The [`query`] function returns a [`Query`] struct. The [`Query`] struct is a builder where
+//! user can bind parameters and change query options.
+//!
+//! ### Encoding
+//!
+//! Having the builder pattern allows for easier multitype parameter instead of having it as
+//! single type array. Parameter must implement [`Encode`], that is the value can be converted
+//! into [`Encoded`] struct. The [`Encoded`] struct can be constructed with value that is one
+//! of rust primitive type as [`ValueRef`], and an postgres [`Oid`].
+//!
+//! ### Query Option
+//!
+//! For now there is only one options that is persistency. All query are prepared,
+//! cached, and reuse for subsequent query. But one can opt out of such behavior.
+//!
+//! ## Fetching
+//!
+//! Fetching define the output of a query. In [`Query`] struct, [`fetch_all`] method will
+//! retrieve all rows as vector. [`fetch_one`] will retrieve one row, or return error otherwise.
+//! [`fetch_optional`] will retrieve optionally retrieve one row.
+//!
+//! Fetch api require a type that can [`Decode`] a set of [`Row`]s.
+
 pub mod common;
 pub mod net;
 

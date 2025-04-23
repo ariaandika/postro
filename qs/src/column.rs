301858634370ed1d70fcefb39193fddf107b2f5c
ariaandika <andikaakun17@gmail.com>
2025-04-23 18:52:18 +0700

    refine Decode for public api

--- qs/src/column.rs
+++ qs/src/column.rs
@@ -3,29 +3,30 @@ use bytes::{Buf, Bytes};
 use crate::{
     common::ByteStr,
     ext::BytesExt,
-    postgres::{backend::RowDescription, Oid},
+    postgres::{Oid, backend::RowDescription},
 };
 
 #[derive(Debug)]
 pub struct ColumnInfo {
     /// The field name.
-    pub field_name: ByteStr,
+    field_name: ByteStr,
     // The object ID of the field's data type.
-    pub data_type: u32,
+    oid: Oid,
 }
 
 impl ColumnInfo {
     pub(crate) fn decode(body: &mut Bytes) -> Result<Self, std::str::Utf8Error> {
+        // <https://www.postgresql.org/docs/current/protocol-message-formats.html#PROTOCOL-MESSAGE-FORMATS-ROWDESCRIPTION>
         let field_name = body.get_nul_bytestr()?;
         body.advance(size_of::<u32>()); // table_oid
         body.advance(size_of::<u16>()); // attribute_len
-        let data_type = body.get_u32();
+        let oid = body.get_u32();
         body.advance(size_of::<i16>()); // data_type_size
         body.advance(size_of::<i32>()); // type_modifier
         body.advance(size_of::<u16>()); // format_code
         Ok(Self {
             field_name,
-            data_type,
+            oid,
         })
     }
 
@@ -36,24 +37,54 @@ impl ColumnInfo {
         }
         Ok(cols)
     }
+
+    pub fn field_name(&self) -> &str {
+        &self.field_name
+    }
+
+    pub fn oid(&self) -> Oid {
+        self.oid
+    }
 }
 
+/// Column information.
 #[derive(Debug)]
 pub struct Column {
-    pub(crate) oid: Oid,
-    pub(crate) value: Bytes,
+    oid: Oid,
+    value: Bytes,
 }
 
 impl Column {
     pub(crate) fn new(col: &ColumnInfo, value: Bytes) -> Self {
         Self {
-            oid: col.data_type,
+            oid: col.oid,
             value,
         }
     }
+
+    /// Get the column [`Oid`].
+    pub fn oid(&self) -> Oid {
+        self.oid
+    }
+
+    /// Get the column value as slice.
+    pub fn as_slice(&self) -> &[u8] {
+        &self.value
+    }
+
+    /// Consume the column value as [`Bytes`].
+    pub fn into_value(self) -> Bytes {
+        self.value
+    }
 }
 
+/// Type that can be used for indexing column.
+///
+/// implemented for
+/// - `&str`
+/// - `usize`
 pub trait Index: Sized {
+    /// Returns the column index.
     fn position(self, cols: &[ColumnInfo]) -> Option<usize>;
 }
 
--- qs/src/decode.rs
+++ qs/src/decode.rs
@@ -1,7 +1,8 @@
 use crate::{Error, Result, column::Column, postgres::PgType};
 
-
+/// Type that can be decoded from column value.
 pub trait Decode: Sized {
+    /// Construct self from a column.
     fn decode(col: Column) -> Result<Self>;
 }
 
@@ -13,21 +14,21 @@ impl Decode for () {
 
 impl Decode for i32 {
     fn decode(col: Column) -> Result<Self> {
-        if col.oid != i32::OID {
+        if col.oid() != i32::OID {
             return Err(Error::MissmatchDataType);
         }
-        let mut be = [0u8;4];
-        be.copy_from_slice(&col.value[..4]);
+        let mut be = [0u8;size_of::<i32>()];
+        be.copy_from_slice(&col.as_slice()[..size_of::<i32>()]);
         Ok(i32::from_be_bytes(be))
     }
 }
 
 impl Decode for String {
     fn decode(col: Column) -> Result<Self> {
-        if col.oid != String::OID {
+        if col.oid() != String::OID {
             return Err(Error::MissmatchDataType);
         }
-        Ok(String::from_utf8(col.value.into()).unwrap())
+        Ok(String::from_utf8(col.into_value().into()).map_err(|e|e.utf8_error())?)
     }
 }
 

57a38f958f8a13d60f12a7f1721699146f169bb5
ariaandika <andikaakun17@gmail.com>
2025-04-30 18:05:37 +0700

    make verbose log as macro

--- qs/src/common/mod.rs
+++ qs/src/common/mod.rs
@@ -6,3 +6,11 @@ mod url;
 pub use bytestr::ByteStr;
 pub(crate) use url::{Url, ParseError};
 
+macro_rules! trace {
+    ($($tt:tt)*) => {
+        #[cfg(feature = "log-verbose")] log::trace!($($tt)*)
+    };
+}
+
+pub(crate) use trace;
+

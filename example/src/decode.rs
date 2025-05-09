f38cb126d86191743074615ac0b51a51fad7abb8
ariaandika <andikaakun17@gmail.com>
2025-05-09 18:36:58 +0700

    add more example for Decode and Encode

--- example/src/decode.rs
+++ example/src/decode.rs
@@ -1,6 +1,15 @@
-use postro::{query_as, types::Json, Connection, Result};
+use postro::{Connection, Decode, Encode, Result, query, query_as, query_scalar, types::Json};
 use serde::Deserialize;
 use time::{PrimitiveDateTime, UtcDateTime};
+
+#[derive(Decode, Encode)]
+struct MyId(i32);
+
+#[derive(Encode)]
+struct MyId2<'a>(&'a str);
+
+#[derive(Decode)]
+struct SomeId<T>(T);
 
 #[derive(Debug, Deserialize, PartialEq, Eq)]
 struct Foo {
@@ -10,9 +19,21 @@ struct Foo {
 pub async fn main() -> Result<()> {
     let mut conn = Connection::connect_env().await?;
 
+    let my_id = MyId(420);
+
+    let _ = query("", &mut conn).bind(&my_id);
+
     let (null,): (Option<String>,) = query_as("SELECT NULL::TEXT", &mut conn).fetch_one().await?;
 
     assert!(null.is_none());
+
+    let my_id: MyId = query_scalar("SELECT 420", &mut conn).fetch_one().await?;
+
+    assert_eq!(my_id.0, 420);
+
+    let some_id: SomeId<i32> = query_scalar("SELECT 420", &mut conn).fetch_one().await?;
+
+    assert_eq!(some_id.0, 420);
 
     // `time`
 
